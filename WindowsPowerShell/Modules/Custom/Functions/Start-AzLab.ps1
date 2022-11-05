b8a0534f09b8dda8d73698cc6a6139cffaeadd27
Julien Masnada <rostskadat@gmail.com>
2022-11-05 10:16:34 +0100

    Added Unix Support

--- WindowsPowerShell/Modules/Custom/Functions/Start-AzLab.ps1
+++ WindowsPowerShell/Modules/Custom/Functions/Start-AzLab.ps1
@@ -19,12 +19,10 @@ function Start-AzLab {
     #>
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
+        [Parameter()]
         [string]
         $Username,
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
+        [Parameter()]
         [string]
         $Password,
         [Parameter()]
@@ -32,27 +30,57 @@ function Start-AzLab {
         $Location = 'East US',
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        $TenantId = '47da9ac4-f585-4753-bf40-7c7c8fe635cf',
+        $TenantId = '47da9ac4-f585-4753-bf40-7c7c8fe635cf', # The "Pluralsight Labs Production" tenant...
         [Parameter()]
         $ResourceGroup
     )
     Set-StrictMode -Version 3
 
-    $SecureString = $Password | ConvertTo-SecureString -AsPlainText -Force
-    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecureString
-    # The "Pluralsight Labs Production" tenant...
-    Connect-AzAccount -Credential $Credential -TenantId $TenantId
-    if ($null -ne $ResourceGroup) {
-        Set-AzDefault -ResourceGroupName $ResourceGroup
+    try {
+        if ($Username -and $Password) {
+            Write-Debug "Connecting as $Username / $TenantId ..."
+            $SecureString = $Password | ConvertTo-SecureString -AsPlainText -Force
+            $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecureString
+            Connect-AzAccount -Credential $Credential -TenantId $TenantId
+        }
+        else {
+            Write-Debug 'Connecting interactively ...'
+            Connect-AzAccount
+        }
+        if ($null -ne $ResourceGroup) {
+            Set-AzDefault -ResourceGroupName $ResourceGroup
+        }
+        $ServicePrincipal = Get-AzADApplication -DisplayName 'WindowsPowerShellLab' | Select-Object -First 1
+        if (-not $ServicePrincipal) {
+            Write-Debug "Creating ServicePrincipal for Lab session as 'Contributor' ..."
+            $ServicePrincipal = New-AzADServicePrincipal -DisplayName 'WindowsPowerShellLab' -Role 'Contributor'
+            $env:ARM_CLIENT_ID = $ServicePrincipal.AppId
+            $env:ARM_CLIENT_SECRET = $ServicePrincipal.PasswordCredentials.SecretText
+        }
+        else {
+            Write-Debug "Reusing existing ServicePrincipal $($ServicePrincipal.Id) ..."
+            # Creating a new credential for that session...
+            $StartDate = Get-Date
+            $EndDate = $startDate.AddYears(1)
+            $AppCredential = New-AzADAppCredential -ObjectId $ServicePrincipal.Id -StartDate $StartDate -EndDate $EndDate
+            $env:ARM_CLIENT_ID = $ServicePrincipal.AppId
+            $env:ARM_CLIENT_SECRET = $AppCredential.SecretText
+        }
+
+        $Subscription = Get-AzSubscription
+        $env:ARM_SUBSCRIPTION_ID = $Subscription.Id
+        $env:ARM_TENANT_ID = $Subscription.TenantId
+        Write-Host -ForegroundColor Green "Once the Lab finished you can call the 'Disconnect-AzAccount' cmdlet" 
+    }
+    catch {
+        Write-Warning $_
     }
 
-    $ServicePrincipal = New-AzADServicePrincipal -DisplayName 'WindowsPowerShellLab' -Role 'Contributor'
-    $Subscription = Get-AzSubscription
+    # Ref: https://geeksarray.com/blog/get-azure-subscription-tenant-client-id-client-secret
+    # AZURE_CLIENT_ID: the application's client ID
+    # AZURE_USERNAME: a username (usually an email address)
+    # AZURE_PASSWORD: that user's password
+    # AZURE_TENANT_ID: (optional) ID of the service principal's tenant. Also called its 'directory' ID. If not provided, defaults to the 'organizations' tenant, which supports only Azure Active Directory work or school accounts.
+    # AZURE_AUTHORITY_HOST: authority of an Azure Active Directory endpoint, for example "login.microsoftonline.com", the authority for Azure Public Cloud, which is the default when no value is given.
 
-    $env:ARM_CLIENT_ID = $ServicePrincipal.AppId
-    $env:ARM_SUBSCRIPTION_ID = $Subscription.Id
-    $env:ARM_TENANT_ID = $Subscription.TenantId
-    $env:ARM_CLIENT_SECRET = $ServicePrincipal.PasswordCredentials.SecretText
-
-    Write-Host -ForegroundColor Green "Once the Lab finished you can call the 'Disconnect-AzAccount' cmdlet" 
 }
--- WindowsPowerShell/Profile.ps1
+++ WindowsPowerShell/Profile.ps1
@@ -16,6 +16,8 @@
 #    }
 #}
 
+$IS_UNIX = ([environment]::OSVersion.Platform -eq 'Unix')
+
 Import-Module -Name Custom
 
 Set-Alias -Name c -Value Clear-Host
@@ -25,16 +27,24 @@ Set-Alias -Name less -Value Format-Page
 Set-Alias -Name gh -Value Get-Help
 Set-Alias -Name ssc -Value Suspend-Computer
 
-New-DynamicVariable GLOBAL:WindowTitle `
--Getter { $host.UI.RawUI.WindowTitle } `
--Setter { $host.UI.RawUI.WindowTitle = $args[0] }
+if (-not $IS_UNIX) {
+    New-DynamicVariable GLOBAL:WindowTitle `
+        -Getter { $host.UI.RawUI.WindowTitle } `
+        -Setter { $host.UI.RawUI.WindowTitle = $args[0] }
+}
 
 # The output of the last output is always available in $__ 
-$PSDefaultParameterValues["Out-Default:OutVariable"] = "__"
+$PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
 
 # Avoid sending telemetry from .NET and AZ Function App toolkit
 $Env:FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT = 1
 
 # Setting up the Oh-my-posh theme
-$Env:POSH_THEMES_PATH = "$Env:USERPROFILE\.poshthemes"
-oh-my-posh init pwsh --config "$Env:POSH_THEMES_PATH\default.omp.yaml" | Invoke-Expression
+if ($IS_UNIX) {
+    $Env:POSH_THEMES_PATH = "$env:HOME/.poshthemes"
+    oh-my-posh init pwsh --config "$Env:POSH_THEMES_PATH/default.omp.yaml" | Invoke-Expression
+}
+else {
+    $Env:POSH_THEMES_PATH = "$Env:USERPROFILE\.poshthemes"
+    oh-my-posh init pwsh --config "$Env:POSH_THEMES_PATH\default.omp.yaml" | Invoke-Expression
+}
